2c60ce839e7c0ce4c111cc2a4c4d95bd1fb57a8b
Ghju <20231695@webmail.universita.corsica>
2026-01-22 16:06:10 +0100

    Ajouts de competences

--- real.js
+++
@@ -1,6 +0,0 @@
-const client = sanityClient({
-    projectId: 'w1hfz6z1',  // Replace with your actual project ID
-    dataset: 'production',  // Replace with your actual dataset
-    useCdn: true
-  });
-  
